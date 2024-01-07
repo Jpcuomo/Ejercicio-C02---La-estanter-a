0de5c72044bcc0309f301118ae03c3d8cfd0a19e
Juan <jpcuomo2000@gmail.com>
2024-01-06 22:46:14 -0300

    Agregar archivos de proyecto.

---
+++ Biblioteca/Estante.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Estante
+    {
+        private Producto[] _productos;
+        private int _ubicacionEstante;
+
+        private Estante(int capacidad)
+        {
+            _productos = new Producto[capacidad];
+        }
+
+        public Estante(int ubicacion, int capacidad) : this(capacidad)
+        {
+            _ubicacionEstante = ubicacion;
+        }
+
+        public Producto[] GetProductos 
+        { 
+            get 
+            { 
+                return _productos; 
+            } 
+        }
+
+        public static string MostrarEstante(Estante estante)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Productos en el estante:");
+            sb.AppendLine($"Ubicación del estante: {estante._ubicacionEstante}");
+            foreach (Producto producto in estante._productos)
+            {
+                sb.AppendLine(Producto.MostrarProducto(producto));
+                sb.AppendLine("---------------------------------------------------");
+            }
+            return sb.ToString();
+        }
+
+        // Sobrecarga de operador == y != para evaluar si el producto se encuentra en el estante o no
+        public static bool operator ==(Estante estante, Producto producto)
+        {
+            return Array.IndexOf(estante.GetProductos, producto) != -1;
+        }
+
+        public static bool operator !=(Estante estante, Producto producto)
+        {
+            return !( estante == producto );
+        }
+
+        // Sobrecarga del operador +. Si hay lugar en el estante y el producto no se encuentra en este lo agrega
+        public static bool operator +(Estante estante, Producto producto)
+        {
+            if (estante != producto)
+            {
+                for(int i = 0; i < estante._productos.Length; i++)
+                {
+                    if (estante._productos[i] is null)
+                    {
+                        estante._productos[i] = producto;
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        // Sobrecarga del operador -.
+        public static Estante operator -(Estante estante, Producto producto)
+        {
+            if(estante == producto)
+            {
+                for (int i = 0; i < estante._productos.Length; i++)
+                {
+                    if((object)producto == estante._productos[i])
+                    {
+                        estante._productos[i] = null;
+                        break;
+                    }
+                }
+            }
+            return estante;
+        }
+    }
+}
---
+++ Biblioteca/Producto.cs
@@ -0,0 +1,66 @@
+﻿using System.Text;
+
+namespace Biblioteca
+{
+    public class Producto
+    {
+        private string _cogidoDeBarra;
+        private string _marca;
+        private float _precio;
+
+        public Producto(string marca, string cogido, float precio)
+        {
+            _marca = marca;
+            _cogidoDeBarra = cogido;
+            _precio = precio;
+        }
+
+        public static string MostrarProducto(Producto producto)
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append($"Marca: {producto.GetMarca()}");
+            sb.Append(" - ");
+            sb.Append($"Precio: {producto.GetPrecio()}");
+            sb.Append(" - ");
+            sb.Append($"Código de barras: {(string)producto}");
+            string cadena = sb.ToString();
+            return cadena;
+        }
+
+        public string GetMarca()
+        {
+            return _marca; 
+        }
+
+        public float GetPrecio()
+        { 
+            return _precio;
+        }
+
+        // Conversión explicita de Producto a string
+        public static explicit operator string(Producto producto)
+        {
+            return producto._cogidoDeBarra;
+        }
+        
+        // Sobrecarga del operador == y != para comparación de Producto-Producto
+        public static bool operator ==(Producto producto1, Producto producto2)
+        {
+            return producto1.GetMarca() == producto2.GetMarca() && producto1._cogidoDeBarra == producto2._cogidoDeBarra;
+        }
+        public static bool operator !=(Producto producto1, Producto producto2)
+        {
+            return !(producto1 == producto2);
+        }
+
+        // Sobrecarga del operador == y != para comparación de Marca y cadena (parametro)
+        public static bool operator ==(Producto producto, string marca)
+        {
+            return (producto.GetMarca() == marca);
+        }
+        public static bool operator !=(Producto producto, string marca)
+        {
+            return !(producto == marca);
+        }
+    }
+}
